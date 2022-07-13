4f453e84b402f0f93e7078e62433706259664f88
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-07-13 11:28:28 +0530

    header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import "./home.css";
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
 };
 
 export default home;
